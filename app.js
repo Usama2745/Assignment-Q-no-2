a35cf9e93abee139decb6c4eb8156eb858864879
usama2745 <nadeemu919@ gmai.com>
2024-03-05 19:15:42 +0500

    Assignment Q no 2

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+let firstname = "hello Eric";
+firstname = "Hello Eric, would you like to learn some python today?";
+console.log(firstname);
---
+++ app.ts
@@ -0,0 +1,7 @@
+//store a person's name in a variable,and print a message to that the person.
+ //Your message should be simple,such as,"Hello Eric,would you like to learn some 
+ //python today?"
+
+let firstname = "hello Eric"
+firstname = "Hello Eric, would you like to learn some python today?"
+console.log(firstname)
